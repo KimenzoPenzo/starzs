6bae60771e930e3f3efbeea84ca15745df7c209d
KimenzoPenzo <100345389+KimenzoPenzo@users.noreply.github.com>
2023-02-11 18:19:10 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,89 +1,94 @@
 <?php
 
-require_once 'app/require.php';
-require_once 'app/controllers/CheatController.php';
+require_once '../app/require.php';
+require_once '../app/controllers/AdminController.php';
 
 $user = new UserController;
-$cheat = new CheatController;
+$admin = new AdminController;
 
 Session::init();
 
-if (!Session::isLogged()) { Util::redirect('/login.php'); }
-
 $username = Session::get("username");
 
-Util::banCheck();
-Util::head($username);
+Util::adminCheck();
+Util::head('Admin Panel');
 Util::navbar();
 
 ?>
 
-<main class="container mt-2">
-
+<div class="container mt-2">
 	<div class="row">
 
-		<!--Welome message-->
-		<div class="col-12 mt-3 mb-2">
-			<div class="alert alert-primary" role="alert">
-				Welcome back, <a href="/profile.php"><b><?php Util::display($username) ?></b></a>.
-			</div>
-		</div>
+		<?php Util::adminNavbar(); ?>
 
 
-		<!--Statistics-->
-		<div class="col-lg-9 col-md-12">
-			<div class="rounded p-3 mb-3">
-				<div class="h5 border-bottom border-secondary pb-1"><i class="fas fa-chart-area"></i> Statistics</div>
-				<div class="row text-muted">
-
-					<!--Total Users-->
-					<div class="col-12 clearfix">
-						Users: <p class="float-right mb-0"><?php Util::display($user->getUserCount()); ?></p>
+		<!--Total Users-->
+		<div class="col-xl-3 col-sm-6 col-xs-12 mt-3">
+			<div class="card">
+				<div class=" card-body row">
+					<div class="col-6 text-center">
+						<h3><i class="fas fa-users fa-2x"></i></h3>
 					</div>
-
-					<!--Latest User-->
-					<div class="col-12 clearfix">
-						Latest User: <p class="float-right mb-0"><?php Util::display($user->getNewUser()); ?></p>
+					<div class="col-6">
+						<h3 class="text-center"><?php Util::display($user->getUserCount()); ?></h3>
+						<span class="small text-muted text-uppercase">total users</span>
 					</div>
-
 				</div>
 			</div>
 		</div>
 
 
-		<!--Status-->
-		<div class="col-lg-3 col-md-12">
-			<div class="rounded p-3 mb-3">
-				<div class="h5 border-bottom border-secondary pb-1"><i class="fas fa-exclamation-circle"></i> Status</div>
-				<div class="row text-muted">
-
-					<!--Detected // Undetected-->
-					<div class="col-12 clearfix">
-						Status: <p class="float-right mb-0"><?php Util::display($cheat->getCheatData()->status); ?></p>
+		
+		<!--Newest registered user-->
+		<div class="col-xl-3 col-sm-6 col-xs-12 mt-3">
+			<div class="card">
+				<div class=" card-body row">
+					<div class="col-6 text-center">
+						<h3><i class="fas fa-user fa-2x"></i></h3>
 					</div>
-
-					<!--Cheat version-->
-					<div class="col-12 clearfix">
-						Version: <p class="float-right mb-0"><?php Util::display($cheat->getCheatData()->version); ?></p>
+					<div class="col-6">
+						<h3 class="text-center text-truncate"><?php Util::display($user->getNewUser()); ?></h3>
+						<span class="small text-muted text-uppercase">latest user</span>
 					</div>
-	
-					<!-- Check if has sub --> 
-					<?php if ($user->getSubStatus() > 0) : ?>
-						<div class="col-12 text-center pt-1">
-							<div class="border-top border-secondary pt-1">
-
-							<a href="/download.php">Download Loader</a>
-							</div>
-						</div>
-					<?php endif; ?>
-
 				</div>
 			</div>
 		</div>
 
 
+		
+		<!--Total banned users-->
+		<div class="col-xl-3 col-sm-6 col-xs-12 mt-3">
+			<div class="card">
+				<div class=" card-body row">
+					<div class="col-6 text-center">
+						<h3><i class="fas fa-user-slash fa-2x"></i></h3>
+					</div>
+					<div class="col-6">
+						<h3 class="text-center"><?php Util::display($user->getBannedUserCount()); ?></h3>
+						<span class="small text-muted text-uppercase">banned users</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		
+		<!--Total active sub users-->
+		<div class="col-xl-3 col-sm-6 col-xs-12 mt-3">
+			<div class="card">
+				<div class=" card-body row">
+					<div class="col-6 text-center">
+						<h3><i class="fas fa-user-clock fa-2x"></i></h3>
+					</div>
+					<div class="col-6">
+						<h3 class="text-center"><?php Util::display($user->getActiveUserCount()); ?></h3>
+						<span class="small text-muted text-uppercase">active sub</span>
+					</div>
+				</div>
+			</div>
+		</div>
 
 	</div>
+</div>
 
-</main>
 <?php Util::footer(); ?>
